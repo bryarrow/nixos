ace12f4037cd16be85082898612acb08ffd2e14c
bryarrow <berry.cn@outlook.com>
2026-07-30 12:54:31 +0800

    改进i18n

--- profiles/i18n/zh-cn.nix
+++ profiles/i18n/zh-cn.nix
@@ -2,7 +2,24 @@
 
 {
   time.timeZone = "Asia/Shanghai";
+
   i18n.defaultLocale = "zh_CN.UTF-8";
+  i18n.supportedLocales = [
+    "C.UTF-8/UTF-8"
+    "en_US.UTF-8/UTF-8"
+    "zh_CN.UTF-8/UTF-8"
+  ];
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "zh_CN.UTF-8";
+    LC_IDENTIFICATION = "zh_CN.UTF-8";
+    LC_MEASUREMENT = "zh_CN.UTF-8";
+    LC_MONETARY = "zh_CN.UTF-8";
+    LC_NAME = "zh_CN.UTF-8";
+    LC_NUMERIC = "zh_CN.UTF-8";
+    LC_PAPER = "zh_CN.UTF-8";
+    LC_TELEPHONE = "zh_CN.UTF-8";
+    LC_TIME = "zh_CN.UTF-8";
+  };
 
   i18n.inputMethod = {
     enable = true;
@@ -18,5 +35,8 @@
     QT_IM_MODULE = "fcitx";
     QT_IM_MODULES = "wayland;fcitx";
     GTK_IM_MODULE = "fcitx";
+
+    LANG = "zh_CN.UTF-8";
+    LC_ALL = "zh_CN.UTF-8";
   };
 }
