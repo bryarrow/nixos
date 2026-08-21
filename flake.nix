257c8a8955cec27ccb87eab3aa4dc38954c58956
bryarrow <berry.cn@outlook.com>
2026-08-21 14:19:33 +0800

    切换到gaokun3-debug分支

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-cuda12.url = "github:NixOS/nixpkgs/0954f7ee2f6bb3dc7d4e3d0d8bcb8fd4bde4cfc5";
    
-    gaokun3.url = "github:bryarrow/linux-gaokun-buildbot/gaokun3-nix";
+    gaokun3.url = "github:bryarrow/linux-gaokun-buildbot/gaokun3-nix-debug";
     gaokun3.inputs.nixpkgs.follows = "nixpkgs";
   };
 
