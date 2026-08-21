d704e63fe84fc16a2d78b8243df4815447676c53
bryarrow <berry.cn@outlook.com>
2026-08-21 14:11:47 +0800

    为EGO添加G15公钥、开放root登陆

--- hosts/Berrys-EGO/users.nix
+++ hosts/Berrys-EGO/users.nix
@@ -5,5 +5,12 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
     shell = pkgs.nushell;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILLAqzFMwuJNgG7rIUNwRij3iqlyXETet8l1424LtnMn berry@Berrys-G15"
+    ];
   };
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILLAqzFMwuJNgG7rIUNwRij3iqlyXETet8l1424LtnMn berry@Berrys-G15"
+  ];
 }
--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -7,7 +7,6 @@
     settings = {
       PasswordAuthentication = true; # 允许密码登录（建议生产环境关闭）
       PermitRootLogin = "prohibit-password"; # 禁止 root 直接用密码登录
-      AllowUsers = [ "berry" ]; # 限制允许登录的用户
     };
   };
 }
